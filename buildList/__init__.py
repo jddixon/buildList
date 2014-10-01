8a51f7822c26ba86ee942bde3a23e329be76616d
Jim Dixon <jddixon@gmail.com>
2014-09-30 17:23:13 -0700

    proj: adding timestamps, base64SHA1File and tests

--- buildList/__init__.py
+++ buildList/__init__.py
@@ -1,11 +1,53 @@
 # buildList/__init__.py
 
+import base64, calendar, hashlib, time
+
 __all__ = ['__version__', '__version_date__',
             # OTHER EXPORTED CONSTANTS
+            'BLOCK_SIZE', 'CONTENT_END', 'CONTENT_START',
             'CRLF',
+            # FUNCTIONS
+            'base64SHA1File',
+            'parseTimestamp', 'timestamp', 'timestampNow', 
           ]
 
 __version__      = '0.1.0'
 __version_date__ = '2014-09-30'
 
-CRLF    = "\r\n"
+BLOCK_SIZE      = 2**18         # 256KB, for no particular reason
+CONTENT_END     = '# END CONTENT #'
+CONTENT_START   = '# START CONTENT '
+CRLF            = "\r\n"
+
+
+# TIMESTAMP FUNCTIONS -----------------------------------------------
+
+# Note that in the Go code timestamp is an int64, whereas here it
+# is a string.
+
+FORMAT = "%Y-%m-%d %H:%M:%S"
+
+def parseTimestamp(s):
+    t = time.strptime(s, FORMAT)
+    return calendar.timegm(t)
+
+def timestamp(n):       # sec from epoch
+    t = time.gmtime(n)
+    return time.strftime(FORMAT,  t)
+
+def timestampNow():
+    t = time.gmtime()
+    return time.strftime(FORMAT,  t)
+
+# SHA1 FILE HASHING -------------------------------------------------
+
+def base64SHA1File(pathToFile):
+    """ This does not as yet cope with errors reading the file. """
+    with open(pathToFile, 'rb') as f:
+        h = hashlib.sha1()
+        while True:
+            block = f.read(BLOCK_SIZE)
+            if not block:
+                break
+            h.update(block)
+    return base64.standard_b64encode(h.digest())
---
+++ testSHA1File.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+# testTimestamp.py
+
+import base64, hashlib, os, time, unittest
+
+from rnglib         import SimpleRNG
+from buildList      import *
+
+class TestTimestamp (unittest.TestCase):
+
+    def setUp(self):
+        self.rng = SimpleRNG( time.time() )
+    def tearDown(self):
+        pass
+
+    # utility functions #############################################
+    
+    # actual unit tests #############################################
+    
+    # Note that in the Go code timestamp is an int64, whereas here it
+    # is a string.
+
+    def testSHA1File (self):
+
+        blkCount = 1 + self.rng.nextInt16(3)     # so 1 to 3
+        # last block will usually be only partically populated
+        byteCount = BLOCK_SIZE * (blkCount - 1) + self.rng.nextInt16(BLOCK_SIZE)
+      
+        print "BLOCK COUNT %d, BYTE COUNT %d\n" % (blkCount, byteCount)
+
+        data = bytearray(byteCount)     # that many null bytes
+        self.rng.nextBytes(data)             # fill with random data
+        d = hashlib.new('sha1')
+        d.update(data)
+        hash = d.digest()
+        b64Hash = base64.standard_b64encode(hash)
+
+        fileName = self.rng.nextFileName(8)
+        pathToFile = os.path.join('tmp', fileName)
+        with open(pathToFile, 'wb') as f:
+            f.write(data)
+
+        fileB64Hash = base64SHA1File(pathToFile)
+
+        #self.assertEquals(b64Hash, fileB64Hash)
+
+
+if __name__ == '__main__':
+    unittest.main()
