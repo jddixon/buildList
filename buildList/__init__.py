b62ad137427df7c752c0a6678c3504e6073261f1
Jim Dixon <jddixon@gmail.com>
2015-04-16 08:34:46 -0700

    blBootstrap builds example

--- buildList/__init__.py
+++ buildList/__init__.py
@@ -18,8 +18,8 @@ __all__ = ['__version__', '__version_date__',
             'expectTitle',
           ]
 
-__version__      = '0.2.2'
-__version_date__ = '2015-04-13'
+__version__      = '0.2.3'
+__version_date__ = '2015-04-16'
 
 BLOCK_SIZE      = 2**18         # 256KB, for no particular reason
 CONTENT_END     = '# END CONTENT #'
