1704ec3081b647be45c499ebb4720785b5c06e6d
Jim Dixon <jddixon@gmail.com>
2016-04-22 16:49:45 -0700

    add .dvcz and projCfg.local

--- buildList/__init__.py
+++ buildList/__init__.py
@@ -32,13 +32,8 @@ __all__ = ['__version__', '__version_date__',
            'BuildList',
            ]
 
-<<<<<<< HEAD
-__version__      = '0.4.11'
-__version_date__ = '2016-04-01'
-=======
-__version__      = '0.4.13'
-__version_date__ = '2016-04-19'
->>>>>>> devel
+__version__      = '0.4.14'
+__version_date__ = '2016-04-22'
 
 BLOCK_SIZE = 2**18         # 256KB, for no particular reason
 CONTENT_END = '# END CONTENT #'
