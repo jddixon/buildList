1bb6b96e35203cb73df736690a04298cfc389593
Jim Dixon <jddixon@gmail.com>
2017-09-26 12:54:11 -0700

    add projConfig.toml; fix setup.py

--- src/buildlist/__init__.py
+++ src/buildlist/__init__.py
@@ -41,8 +41,8 @@ __all__ = ['__version__', '__version_date__',
            'BuildList',
            'BLIntegrityCheckFailure', 'BLParseFailed', 'BLError', ]
 
-__version__ = '0.9.8'
-__version_date__ = '2017-09-11'
+__version__ = '0.9.9'
+__version_date__ = '2017-09-26'
 
 # UTILITY FUNCTIONS -------------------------------------------------
 
