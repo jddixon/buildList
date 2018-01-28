e4984c6b216ff218523462400557fd673522b7a1
Jim Dixon <jddixon@gmail.com>
2018-01-28 15:07:19 -0800

    add checkExample4; add blake2b stuff to xl_testData

--- src/buildlist/__init__.py
+++ src/buildlist/__init__.py
@@ -41,8 +41,8 @@ __all__ = ['__version__', '__version_date__',
            'BuildList',
            'BLIntegrityCheckFailure', 'BLParseFailed', 'BLError', ]
 
-__version__ = '0.10.0'
-__version_date__ = '2018-01-27'
+__version__ = '0.10.1'
+__version_date__ = '2018-01-28'
 
 # UTILITY FUNCTIONS -------------------------------------------------
 
